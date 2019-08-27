dbcff92c1575a3f4fbc20cc176cb7ec58e1aab90
Trevor Vannoy <trevorvannoy@montana.edu>
2019-08-27 09:16:55 -0600

    add custom DSP Builder library blocks

---
+++ add_library_to_path.m
@@ -0,0 +1,4 @@
+% For the library to show up in the simulink library browswer, we need to
+% add this library folder to the matlab path. 
+
+addpath('.')
---
+++ slblocks.m
@@ -0,0 +1,10 @@
+function blkStruct = slblocks
+% This function specifies that the library should appear
+% in the Library Browser
+% and be cached in the browser repository
+
+Browser.Library = 'dpsba_custom';
+
+Browser.Name = 'Custom DSP Builder Advanced blocks';
+
+blkStruct.Browser = Browser; 
