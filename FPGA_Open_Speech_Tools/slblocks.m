f669fc68cca740cea25d2bcff439cba118275d7b
Bailey Galacci <bailey0522@gmail.com>
2020-03-02 19:25:26 -0700

    renaming files/folders to move later

---
+++ FPGA_Open_Speech_Tools/slblocks.m
@@ -0,0 +1,12 @@
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'FPGA_Open_Speech_Tools';
+		
+
+		Browser.Name = 'FPGA_Open_Speech_Tools';
+		
+
+		blkStruct.Browser = Browser; 
